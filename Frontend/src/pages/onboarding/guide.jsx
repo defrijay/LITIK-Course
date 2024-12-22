962cff0220eea93f72e5b42432b5fd89d6f89ec6
Defrijay <defrijay@gmail.com>
2024-12-22 15:19:26 +0700

    tambah dark mode di semua mode guide

--- Frontend/src/pages/onboarding/guide.jsx
+++ Frontend/src/pages/onboarding/guide.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Guide = () => {
   return (
-    <section className="flex-col gap-12 bg-gray-50 dark:bg-slate-950 min-h-screen flex items-center justify-center px-4 sm:px-6 md:px-10 lg:px-16">
+    <section className="flex-col gap-12 bg-slate-950 min-h-screen flex items-center justify-center px-4 sm:px-6 md:px-10 lg:px-16">
       {/* Judul */}
-      <h1 className="text-gray-900 text-3xl sm:text-4xl md:text-5xl lg:text-6xl dark:text-white font-bold text-center">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white font-bold text-center">
         Petunjuk Penggunaan
       </h1>
 
       {/* Deskripsi */}
-      <p className="text-gray-900 text-sm sm:text-base md:text-lg lg:text-xl dark:text-white text-justify max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
+      <p className="text-sm sm:text-base md:text-lg lg:text-xl text-white text-justify max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
         Selamat datang di aplikasi belajar interaktif kami! 🎉 Berikut adalah panduan penggunaan aplikasi ini:
         <br />
         <br />
@@ -30,13 +30,13 @@ const Guide = () => {
       <div className="space-x-2 sm:space-x-4 md:space-x-8 lg:space-x-12 flex w-full justify-between">
         <Link
           to="/"
-          className="px-4 sm:px-6 md:px-8 lg:px-12 py-2 text-xs sm:text-sm md:text-base lg:text-lg rounded-full bg-gradient-to-r from-orange-800 to-orange-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 opacity-50 text-white"
+          className="px-4 sm:px-6 md:px-8 lg:px-12 py-2 text-xs sm:text-sm md:text-base lg:text-lg rounded-full bg-gradient-to-r from-orange-800 to-orange-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-800 opacity-50 text-white"
         >
           Sebelumnya
         </Link>
         <Link
           to="/menu"
-          className="px-4 sm:px-6 md:px-8 lg:px-12 py-2 text-xs sm:text-sm md:text-base lg:text-lg rounded-full bg-gradient-to-r from-orange-600 to-orange-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 text-white"
+          className="px-4 sm:px-6 md:px-8 lg:px-12 py-2 text-xs sm:text-sm md:text-base lg:text-lg rounded-full bg-gradient-to-r from-orange-600 to-orange-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-800 text-white"
         >
           Selanjutnya
         </Link>
