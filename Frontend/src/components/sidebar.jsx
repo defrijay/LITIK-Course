3def3320de168b653d220879d013e043cb755910
Defrijay <defrijay@gmail.com>
2024-12-23 09:19:26 +0700

    tambah opsi Siswa di sidebar

--- Frontend/src/components/sidebar.jsx
+++ Frontend/src/components/sidebar.jsx
@@ -88,6 +88,7 @@ const Sidebar = ({ children }) => {
                                 <span className="ms-3">Dashboard</span>
                             </Link>
                         </li>
+
                         <li>
                             <button
                                 type="button"
@@ -144,6 +145,18 @@ const Sidebar = ({ children }) => {
                                     </li>
                                 </ul>
                             )}
+                        </li>
+                        <li>
+                            <Link
+                                to="/user"
+                                className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
+                            >
+                                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3 1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
+                                </svg>
+
+                                <span className="ms-3">Siswa</span>
+                            </Link>
                         </li>
                         {/* More sidebar items */}
                     </ul>
