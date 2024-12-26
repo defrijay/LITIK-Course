c2c8efae0b7bcd05d1b9c473573fa35534866d68
Defrijay <defrijay@gmail.com>
2024-12-27 02:14:40 +0700

    benarkan backend

--- Frontend/src/components/sidebar.jsx
+++ Frontend/src/components/sidebar.jsx
@@ -148,7 +148,7 @@ const Sidebar = ({ children }) => {
                         </li>
                         <li>
                             <Link
-                                to="api/user"
+                                to="/users"
                                 className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group"
                             >
                                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
