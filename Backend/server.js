1bfb98c4c4504baabcbea7b38669e23660196a4a
Defrijay <defrijay@gmail.com>
2024-12-28 14:01:06 +0700

    tambahkan skor untuk evaluasi

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
@@ -8,9 +7,9 @@ const app = express();
 
 // Middleware CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || "https://litik-course.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: "https://litik-course.vercel.app", // Origin frontend yang diizinkan
+  methods: ["GET", "POST", "PUT", "DELETE"], // HTTP methods yang diizinkan
+  credentials: true, // Jika menggunakan cookie atau header otentikasi
 }));
 
 // Middleware lainnya
@@ -26,12 +25,6 @@ app.get("/api", (req, res) => {
 
 // Default route
 app.get("/", (req, res) => res.send("Express API running on Vercel"));
-
-// Handler kesalahan global
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-});
 
 // Export app for Vercel
 module.exports = app;
